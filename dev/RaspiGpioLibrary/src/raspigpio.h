076cc7693019ff59a093e9291be7c8d5dd21f909
CountrySideEngineer <deep-sleeper@nifty.com>
2020-03-12 22:01:54 +0900

    Add headers which integrates the headers file the source codes which use the library should include.

---
+++ dev/RaspiGpioLibrary/src/raspigpio.h
@@ -0,0 +1,18 @@
+/*
+ * raspigpio.h
+ *
+ *  Created on: 2020/03/12
+ *      Author: CountrySideEngineer
+ */
+
+#ifndef RASPIGPIO_H_
+#define RASPIGPIO_H_
+
+/*
+ * Include headers should be included source code the library uses.
+ */
+#include "CGpio.h"
+#include "CPart.h"
+#include "CSpi.h"
+
+#endif /* RASPIGPIO_H_ */
